3d3edec33bfe77414ff9923a48d144a447c8fd49
AxelDumon <axel.dumon.etu@univ-lille.fr>
2025-11-18 18:32:12 +0100

    added mongo base full management

--- server/models/BaseManager/MongoManager.ts
+++ server/models/BaseManager/MongoManager.ts
@@ -15,14 +15,17 @@ dotenv.config();
 export class MongoManager extends BaseManager {
   cellRepository: CellMongoRepository;
   agentRepository: AgentMongoRepository;
-  client: MongoClient;
+  mainClient: MongoClient;
+  secondaryClient: MongoClient;
   db: Db;
+  connectionType: "repl" | "standalone" = "repl";
 
   constructor() {
     super();
     this.cellRepository = new CellMongoRepository({} as Collection<Cell>);
     this.agentRepository = new AgentMongoRepository({} as Collection<Agent>);
-    this.client = {} as MongoClient;
+    this.mainClient = {} as MongoClient;
+    this.secondaryClient = {} as MongoClient;
     this.db = {} as Db;
   }
 
@@ -31,6 +34,12 @@ export class MongoManager extends BaseManager {
     const manager = new MongoManager();
     await manager.connectToDatabase();
     await manager.initBase();
+    setInterval(async () => {
+      await manager.checkReplicaSetHealth();
+    }, 3000);
+    setInterval(async () => {
+      await manager.replicateDataToStandalone();
+    }, 5000);
     return manager;
   }
 
@@ -44,12 +53,15 @@ export class MongoManager extends BaseManager {
   }
 
   async connectToDatabase(): Promise<void> {
-    let uri = process.env.REPL_MONGO_URI;
+    let uri =
+      this.connectionType === "repl"
+        ? process.env.REPL_MONGO_URI
+        : process.env.STANDALONE_MONGO_URI || "mongodb://localhost:27017";
     let options: any = { serverApi: { version: "1" } };
     try {
-      this.client = new MongoClient(uri!, options);
-      await this.client.connect();
-      this.db = this.client.db("v2grid");
+      this.mainClient = new MongoClient(uri!, options);
+      await this.mainClient.connect();
+      this.db = this.mainClient.db("v2grid");
       console.log("Connected to database:", this.db.databaseName);
 
       this.cellRepository = new CellMongoRepository(
@@ -151,6 +163,116 @@ export class MongoManager extends BaseManager {
     } catch (error) {
       console.error("Failed to get simulation stats:", error);
       throw error;
+    }
+  }
+
+  async checkReplicaSetHealth() {
+    const replUri = process.env.REPL_MONGO_URI;
+    const testClient = new MongoClient(replUri!);
+
+    try {
+      await testClient.connect();
+      const adminDb = testClient.db("admin");
+
+      // rs.status()
+      const status = await adminDb.command({ replSetGetStatus: 1 });
+
+      // Check the members' states
+      const healthyMembers = status.members.filter((member: any) =>
+        ["PRIMARY", "SECONDARY"].includes(member.stateStr)
+      );
+
+      if (healthyMembers.length >= 3 && this.connectionType === "standalone") {
+        console.log(
+          "Replica set is healthy with at least 3 members. Switching to repl mode..."
+        );
+        this.connectionType = "repl";
+        await this.mainClient.close();
+        await this.connectToDatabase();
+        this.secondaryClient = new MongoClient(
+          process.env.STANDALONE_MONGO_URI || "mongodb://localhost:27017"
+        );
+        await this.secondaryClient.connect();
+        await this.replicateDataToRepl();
+      } else {
+        console.error(
+          "Replica set is unhealthy. Less than 3 members are healthy. Switching to standalone mode..."
+        );
+        this.connectionType = "standalone";
+        await this.secondaryClient.close();
+        await this.mainClient.close();
+        await this.connectToDatabase();
+      }
+    } catch (error) {
+      console.error("Failed to check replica set health:", error);
+    } finally {
+      await testClient.close();
+    }
+  }
+
+  async replicateDataToStandalone(): Promise<void> {
+    if (this.connectionType !== "repl") {
+      console.log("Not in repl mode. Skipping data replication.");
+      return;
+    }
+
+    try {
+      await this.secondaryClient.connect();
+      const standaloneDb = this.secondaryClient.db("v2grid");
+      const cells = await this.cellRepository.findAll();
+      const agents = await this.agentRepository.findAll();
+      // Replicate Cells
+
+      const standaloneCellRepo = new CellMongoRepository(
+        standaloneDb.collection("cells")
+      );
+      await standaloneCellRepo.getCollection().deleteMany({});
+      if (cells.length > 0) {
+        await standaloneCellRepo.getCollection().insertMany(cells);
+      }
+
+      // Replicate Agents
+      const standaloneAgentRepo = new AgentMongoRepository(
+        standaloneDb.collection("agents")
+      );
+      await standaloneAgentRepo.getCollection().deleteMany({});
+      if (agents.length > 0) {
+        await standaloneAgentRepo.getCollection().insertMany(agents);
+      }
+
+      console.log("Data replication to standalone MongoDB completed.");
+    } catch (error) {
+      console.error("Failed to replicate data to standalone MongoDB:", error);
+    }
+  }
+
+  async replicateDataToRepl(): Promise<void> {
+    if (this.connectionType !== "repl") {
+      console.log("Not in repl mode. Skipping data replication.");
+      return;
+    }
+    try {
+      await this.secondaryClient.connect();
+      const replDb = this.secondaryClient.db("v2grid");
+      const distantCells = await this.cellRepository.findAll();
+
+      // Replicate Cells
+      const replCellRepo = new CellMongoRepository(replDb.collection("cells"));
+      const localCells = await replCellRepo.findAll();
+      const cellsToInsert = distantCells.filter(
+        (distantCell) =>
+          !localCells.some(
+            (localCell) =>
+              localCell.x === distantCell.x && localCell.y === distantCell.y
+          )
+      );
+      if (cellsToInsert.length > 0) {
+        await this.cellRepository.getCollection().insertMany(cellsToInsert);
+      }
+
+      console.log("Data replication to repl MongoDB completed.");
+    } catch (error) {
+      console.error("Failed to replicate data to repl MongoDB:", error);
     }
   }
 }
